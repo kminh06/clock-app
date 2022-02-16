aafa8fd0aa1f1f80ad1ee0af09b092b55c9a2bd6
KMinh <daukhacminh006@gmail.com>
2022-02-16 16:44:10 +0700

    Adding Copyright

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div id="footer">Copyright &copy; 2022 Khac Minh Dau</div>
+    <div id="footer">COPYRIGHT &copy; 2022 KHAC MINH DAU</div>
   )
 }
